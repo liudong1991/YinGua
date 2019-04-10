7891c2b69b3abcf5ac25eb346b262e5ed0532f70
liudong <liudongwust@gmail.com>
2019-04-10 18:06:45 +0800

    afsdfsfsdf

--- app/src/main/java/club/wustfly/inggua/ui/fragment/MainPageFragment.java
+++ app/src/main/java/club/wustfly/inggua/ui/fragment/MainPageFragment.java
@@ -6,9 +6,13 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -24,6 +28,7 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -52,8 +57,12 @@ public class MainPageFragment extends BaseFragment {
     TextView service_status_txt;
     @BindView(R.id.re_locate_btn)
     TextView re_locate_btn;
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
 
-    //Integer[] imgs = new Integer[]{R.mipmap.banner1, R.mipmap.banner1, R.mipmap.banner1, R.mipmap.banner1, R.mipmap.banner1};
+    Integer[] imgs = new Integer[]{R.mipmap.banner1, R.mipmap.banner1, R.mipmap.banner1, R.mipmap.banner1, R.mipmap.banner1};
+
+    List<BannerItem> list = new ArrayList<>();
 
     @Nullable
     @Override
@@ -91,6 +100,26 @@ public class MainPageFragment extends BaseFragment {
         banner.setIndicatorRes(R.mipmap.banner_unselected_indicator, R.mipmap.banner_selected_indicator);
 
         getBannerImg();
+
+        viewPager.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = viewPager.getWidth();
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeResource(getResources(), R.mipmap.banner_model, options);
+                int height = (int) (width * 1.0 / options.outWidth * options.outHeight);
+                //Log.i("wust-lz", "wust-ls===>viewpager width:" + width);
+                ViewGroup.LayoutParams lp = viewPager.getLayoutParams();
+                lp.width = width + 20;
+                lp.height = height;
+                viewPager.setLayoutParams(lp);
+            }
+        });
+
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.setAdapter(adapter);
+
     }
 
     private void getBannerImg() {
@@ -108,6 +137,10 @@ public class MainPageFragment extends BaseFragment {
                 return new BannerViewHolder();
             }
         });
+
+        list.clear();
+        list.addAll(bannerItemList);
+        adapter.notifyDataSetChanged();
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
@@ -156,14 +189,64 @@ public class MainPageFragment extends BaseFragment {
         @Override
         public void onBind(Context context, int position, BannerItem data) {
 
-            RequestOptions options = new RequestOptions();
-
             Glide.with(MainPageFragment.this)
                     .load(Constants.BASE_URL + data.getImage())
-                    .placeholder(R.mipmap.banner1)
+                    .placeholder(R.mipmap.banner_model)
                     .into(mImageView);
-
-
         }
     }
+
+    private PagerAdapter adapter = new PagerAdapter() {
+
+        private final int mLooperCountFactor = 500;
+
+        private int getStartSelectItem() {
+            if (getRealCount() == 0) {
+                return 0;
+            }
+            // 我们设置当前选中的位置为Integer.MAX_VALUE / 2,这样开始就能往左滑动
+            // 但是要保证这个值与getRealPosition 的 余数为0，因为要从第一页开始显示
+            int currentItem = getRealCount() * mLooperCountFactor / 2;
+            if (currentItem % getRealCount() == 0) {
+                return currentItem;
+            }
+            // 直到找到从0开始的位置
+            while (currentItem % getRealCount() != 0) {
+                currentItem++;
+            }
+            return currentItem;
+        }
+
+        private int getRealCount() {
+            return list == null ? 0 : list.size();
+        }
+
+        @Override
+        public int getCount() {
+            return getRealCount() * mLooperCountFactor;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            final int realPosition = position % getRealCount();
+            ImageView iv = new ImageView(getContext());
+            container.addView(iv);
+            BannerItem data = list.get(realPosition);
+            Glide.with(MainPageFragment.this)
+                    .load(Constants.BASE_URL + data.getImage())
+                    .placeholder(/*R.mipmap.banner_model*/R.mipmap.banner1)
+                    .into(iv);
+            return iv;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    };
 }
